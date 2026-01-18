131adc15c82822f6bcdf99fb0653a5371bd59d92
PEZ <pez@pezius.com>
2026-01-18 13:38:44 +0100

    Extract fs actions

---
+++ src/background_actions/repl_fs_actions.cljs
@@ -0,0 +1,147 @@
+(ns background-actions.repl-fs-actions
+  "Action implementations for background REPL FS operations.
+   No Chrome APIs, no atoms, no side effects - just state transitions."
+  (:require [script-utils :as script-utils]))
+
+;; ============================================================
+;; Helper Functions (Pure)
+;; ============================================================
+
+(defn find-script-by-name
+  "Find a script by name in the scripts list."
+  [scripts name]
+  (->> scripts
+       (filter #(= (:script/name %) name))
+       first))
+
+(defn find-script-by-id
+  "Find a script by ID in the scripts list."
+  [scripts id]
+  (->> scripts
+       (filter #(= (:script/id %) id))
+       first))
+
+(defn- make-error-response
+  "Create a failure response with error message."
+  [error-msg]
+  {:uf/fxs [[:bg/fx.send-response {:success false :error error-msg}]]})
+
+(defn- make-success-response
+  "Create a success response with optional extra data."
+  ([updated-scripts]
+   (make-success-response updated-scripts {}))
+  ([updated-scripts extra]
+   {:uf/db {:storage/scripts updated-scripts}
+    :uf/fxs [[:storage/fx.persist!]
+             [:bg/fx.broadcast-scripts-changed!]
+             [:bg/fx.send-response (merge {:success true} extra)]]}))
+
+(defn- update-script-in-list
+  "Update a script in the list by ID, applying update-fn."
+  [scripts script-id update-fn]
+  (mapv (fn [s]
+          (if (= (:script/id s) script-id)
+            (update-fn s)
+            s))
+        scripts))
+
+(defn- remove-script-from-list
+  "Remove a script from the list by ID."
+  [scripts script-id]
+  (filterv #(not= (:script/id %) script-id) scripts))
+
+;; ============================================================
+;; Action Implementations
+;; ============================================================
+
+(defn rename-script
+  "Rename a script by name."
+  [{:fs/keys [scripts now-iso from-name to-name]}]
+  (let [source-script (find-script-by-name scripts from-name)]
+    (cond
+      ;; Source not found
+      (nil? source-script)
+      (make-error-response (str "Script not found: " from-name))
+
+      ;; Source is builtin
+      (script-utils/builtin-script? source-script)
+      (make-error-response "Cannot rename built-in scripts")
+
+      ;; Target name exists
+      (find-script-by-name scripts to-name)
+      (make-error-response (str "Script already exists: " to-name))
+
+      ;; All checks pass - allow rename
+      :else
+      (let [updated-scripts (update-script-in-list scripts (:script/id source-script)
+                              #(assoc % :script/name to-name :script/modified now-iso))]
+        (make-success-response updated-scripts
+                               {:from-name from-name :to-name to-name})))))
+
+(defn delete-script
+  "Delete a script by name."
+  [{:fs/keys [scripts script-name]}]
+  (let [script (find-script-by-name scripts script-name)]
+    (cond
+      ;; Script not found
+      (nil? script)
+      (make-error-response (str "Script not found: " script-name))
+
+      ;; Script is builtin
+      (script-utils/builtin-script? script)
+      (make-error-response "Cannot delete built-in scripts")
+
+      ;; All checks pass - allow delete
+      :else
+      (let [updated-scripts (remove-script-from-list scripts (:script/id script))]
+        (make-success-response updated-scripts
+                               {:name script-name})))))
+
+(defn save-script
+  "Create or update a script."
+  [{:fs/keys [scripts now-iso script]}]
+  (let [script-id (:script/id script)
+        script-name (:script/name script)
+        force? (:script/force? script)
+        existing-by-id (find-script-by-id scripts script-id)
+        existing-by-name (find-script-by-name scripts script-name)
+        is-update? (some? existing-by-id)]
+    (cond
+      ;; Trying to update a builtin script (by ID)
+      (and is-update? (script-utils/builtin-script-id? script-id))
+      (make-error-response "Cannot modify built-in scripts")
+
+      ;; Trying to overwrite a builtin script (by name, even with force)
+      (and existing-by-name (script-utils/builtin-script? existing-by-name))
+      (make-error-response "Cannot overwrite built-in scripts")
+
+      ;; Trying to save with a name that would shadow a builtin (via normalization)
+      (script-utils/name-matches-builtin? scripts script-name)
+      (make-error-response "Cannot overwrite built-in scripts")
+
+      ;; Name collision on create (different ID, same name, no force)
+      (and (not is-update?)
+           existing-by-name
+           (not force?))
+      (make-error-response (str "Script already exists: " script-name))
+
+      ;; All checks pass - create or update
+      :else
+      (let [;; Remove force? flag before storing
+            clean-script (dissoc script :script/force?)
+            ;; Add timestamps
+            timestamped-script (if is-update?
+                                 (assoc clean-script :script/modified now-iso)
+                                 (assoc clean-script
+                                        :script/created now-iso
+                                        :script/modified now-iso))
+            updated-scripts (if is-update?
+                              ;; Update existing
+                              (update-script-in-list scripts script-id (constantly timestamped-script))
+                              ;; Create new (remove any with same name if force)
+                              (let [filtered (if (and force? existing-by-name)
+                                               (remove-script-from-list scripts (:script/id existing-by-name))
+                                               scripts)]
+                                (conj filtered timestamped-script)))]
+        (make-success-response updated-scripts
+                               {:name script-name :is-update is-update?})))))
